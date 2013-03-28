975882bd201955db66b39fa9a26c4484db15f90a
milaq <micha.laqua@gmail.com>
2013-03-28 04:21:06 +0100

    add all necessary stuff to build for kovsky

---
+++ build_kovsky.sh
@@ -0,0 +1,22 @@
+# using codesourcery arm-2009q3-67
+export PATH=${PATH}:./tools/HTCFlasher-20080806/src/:~/android_toolchains/arm-2009q3/bin/
+export CROSS_COMPILE=arm-none-linux-gnueabi-
+export TOOLCHAIN_PREFIX=arm-none-linux-gnueabi-
+export AS=${CROSS_COMPILE}as
+export OBJCOPY=${CROSS_COMPILE}objcopy
+
+make msm7200a_htc_wince clean
+make msm7200a_htc_wince
+
+cp ./build-msm7200a_htc_wince/lk.bin .
+cp ./build-msm7200a_htc_wince/lk.bin ./tools/
+
+pushd ./tools/
+$AS tinboot.S -o tinboot.o
+$OBJCOPY tinboot.o -O binary tinboot
+gcc generate.c -o generate
+./generate
+yang -F lk.nbh -f lk.nb -t 0x400 -s 64 -d KOVS***** -c 11111111 -v 1.0.XDAPOOP -l WWE
+cp lk.nbh ../kovsimg.nbh
+rm lk.bin
+popd
